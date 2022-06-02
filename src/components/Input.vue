e360d5cdb725afbd2705aa5618c19b5731b7779f
gdantasc <gabrieldcavalcante00@gmail.com>
2022-06-02 15:34:51 -0300

    feat: create input component

---
+++ src/components/Input.vue
@@ -0,0 +1,27 @@
+<template>
+  <input type="text" placeholder="Search" />
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+
+  $gray:#5f5f5f;
+
+input {
+  width: 95%;
+  background: transparent;
+  border: none;
+  border-bottom: 1px solid $gray;
+  margin: 20px 0;
+  padding: 10px;
+  font-size: 1rem;
+  color: $gray;
+
+  &:focus {
+  outline: none;
+  }
+}
+</style>
