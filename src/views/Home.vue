5d0456e3d15e2841ebd1433a6fa96fb373cd2623
gdantasc <gabrieldcavalcante00@gmail.com>
2022-06-02 15:35:29 -0300

    refactor: Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,66 @@
 <template>
-  <h1>Hello World</h1>
+  <div class="container">
+    <Input @input="filter = $event.target.value" />
+    <Card
+      v-for="pokemon in filteredPokemons"
+      :key="pokemon.id"
+      :pokemon="pokemon"
+    />
+  </div>
 </template>
 
 <script>
-export default {
+import Card from "../components/Card.vue";
+import Input from "../components/Input.vue";
+import { http } from "../services/config";
+import { PAGINATION_MAX } from "../constants/axios";
+import { IMAGE_URL } from "../constants/references";
 
-}
+export default {
+  name: "App",
+  components: {
+    Card,
+    Input,
+  },
+  data: () => ({
+    pokemons: [],
+    filter: "",
+  }),
+  async mounted() {
+    const response = await http.get("pokemon/", {
+      params: { limit: PAGINATION_MAX },
+    });
+
+    this.pokemons = response.data.results.map((pokemon, index) => ({
+      ...pokemon,
+      index: index + 1,
+      name: `${pokemon.name.charAt(0).toUpperCase()}${pokemon.name.slice(1)} `,
+      image: `${IMAGE_URL}${index + 1}.png`,
+    }));
+  },
+  computed: {
+    filteredPokemons() {
+      if (this.filter) {
+        const pokemons = this.pokemons.filter((pokemon) => {
+          return pokemon.name
+            .toLowerCase()
+            .startsWith(this.filter.toLowerCase());
+        });
+        return pokemons;
+      }
+      return this.pokemons;
+    },
+  },
+};
 </script>
 
-<style>
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 10px;
+}
 
 </style>
