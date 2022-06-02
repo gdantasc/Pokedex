f1c8e7af4f575922bf8a1d7adb5875c08c7e333d
gdantasc <gabrieldcavalcante00@gmail.com>
2022-06-02 15:35:10 -0300

    refactor: Card component

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,13 +1,49 @@
 <template>
-   <div>Hello world</div>
+  <div class="card-container">
+    <div class="img-container">
+      <img :src="`${pokemon.image}`" :alt="pokemon.name" />
+    </div>
+    <div class="text-container">
+      <p>{{ pokemon.name }}</p>
+      <p>#{{ pokemon.index }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  props: ["pokemon"],
+};
 </script>
 
-<style>
+<style lang="scss" scoped>
+.card-container {
+  width: 250px;
+  height: 150px;
+  padding: 10px;
+  border-radius: 3px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #fff;
+}
+.text-container {
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
 
+  p {
+    font-size: 0.9rem;
+    color: #2e2e2e;
+  }
+}
+.img-container {
+  display: flex;
+  align-items: center;
+
+  img {
+    width: 110px;
+  }
+}
 </style>
